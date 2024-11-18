156ccba311aad671fd1e0c9317ce5cdf5efa015b
Thiago Mello <timabuntu@gmail.com>
2024-11-18 20:12:34 -0300

    initialized project drag and drop notes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NotesPage from "./pages/NotesPage";
+function App() {
+  return (
+    <>
+      <div id="app">
+        <NotesPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/NotesPage.jsx
@@ -0,0 +1,10 @@
+
+const NotesPage = () => {
+  return (
+    <div>
+      <h1>NotesPage</h1>
+    </div>
+  )
+}
+
+export default NotesPage
